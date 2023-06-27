6b7c570ff385bfeafaacdeb6a9fdf9fc8aba6804
f00lg0ldl0af <43835575+f00lg0ldl0af@users.noreply.github.com>
2023-06-27 11:42:08 +0800

    Add function game

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
+// Declare variables
+
+
 // Function returns either 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice () {
     const computerSelection = ['Rock', 'Paper', 'Scissors'];
     return computerSelection[Math.floor(Math.random()*computerSelection.length)];  
 }
-
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
 
 // Function ensures player enters valid characters only
-function alphaCharOnly(inputText) {
+function alphaCharOnly(playerSelection) {
     let letters = /^[a-zA-Z]+$/;
-    if(letters.test(inputText)) {
+    if(letters.test(playerSelection)) {
         return true;
     } else {
         alert('Invalid input. Please enter alpha characters only.');
         return false;
     }
 }
-console.log(alphaCharOnly())
+//console.log(alphaCharOnly())
 
 // Function returns player choice. 
 function getPlayerChoice() {
@@ -30,16 +32,14 @@ function getPlayerChoice() {
 
 }
 
-console.log(getPlayerChoice())
+//console.log(getPlayerChoice())
 
 // Function plays a single round of Rock Paper Scissors and declares winner of the round.
 
 function playRound (playerSelection, computerSelection) {
-    
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-    if (playerSelection === getComputerChoice) {
-        return "It's a tie between you and computer.";
+    if (playerSelection === computerSelection) {
+        return `It's a tie. You chose ${playerSelection}. Computer chose ${computerSelection}.`;
     } else if (playerSelection === "Rock" && computerSelection === "Scissors" ) {
         return `You won. ${playerSelection} beats ${computerSelection}.` 
     } else if (playerSelection === "Rock" && computerSelection === "Paper" ) {
@@ -55,7 +55,44 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
-//Problem: returns undefined
+//const playerSelection = "Rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection)) 
+
+// Function plays a 5-round game that keep scores and reports a winner or a loser at the end.
+function game() {
+    let playerScore =  0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 6; i++){
+        const playerSelection = getPlayerChoice();
+        console.log(playerSelection)
+
+        const computerSelection = getComputerChoice();
+        console.log(computerSelection)
+        
+        const results = playRound(playerSelection,computerSelection);
+        console.log(results)
+
+        if (results === `You won. ${playerSelection} beats ${computerSelection}.`) {
+            playerScore++;
+        } else if (results === `It's a tie. You chose ${playerSelection}. Computer chose ${computerSelection}.`) {
+            playerScore;
+            computerScore;
+        } else {
+            computerScore++;
+        }
+    console.log("Player Score: ", playerScore)
+    console.log("Computer Score: ", computerScore)   
+    }
+    
+    if (playerScore > computerScore) {
+        return "Player won."
+    } else if (playerScore < computerScore) {
+        return "Computer won."
+    } else {
+        "It's a tie."
+    }
+}
+//console.log(game())
+
